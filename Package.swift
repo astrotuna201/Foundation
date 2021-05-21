6fd47078b96148efb8014188cd750aa22d3c84fe
hpaelike <hpaelike@marum.de>
2021-05-21 18:26:22 +0200

    ..:

--- Package.swift
+++ Package.swift
@@ -33,5 +33,5 @@ target.exclude += [
 pkg.targets = [target]
 
 pkg.platforms = [
-   .macOS(.v10_10), .iOS(.v13), .tvOS(.v9), .watchOS(.v2)
+   .macOS(.v10_12), .iOS(.v13), .tvOS(.v9), .watchOS(.v2)
 ]
